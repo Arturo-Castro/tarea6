2e8520b473aa5c63b9eeddd373a6655a874370fb
Arturo Castro <castro.ah91@gmail.com>
2020-11-28 15:09:44 -0300

    cambios
    modificacion y terminacion tarea 1 de la clase 6

--- tarea6.js
+++ tarea6.js
@@ -13,6 +13,36 @@ function crearBr(){
 }
 
 
+function calcularPromedioArray(array){
+    let suma = 0;
+    let promedio;
+    for(let i=0; i<array.length; i++){
+        suma += array[i];
+    }promedio = suma / array.length;
+    return promedio;
+}
+
+
+function obtenerMinimo(array){
+    let arrayMinimo = array[0];
+    for(let i=1; i<array.length; i++){
+        if (array[i] < arrayMinimo){
+            arrayMinimo = array[i];
+        }  
+    }return arrayMinimo;   
+    
+}
+
+
+function obtenerMaximo(array){
+    let arrayMaximo = array[0];
+    for(let i=1; i<array.length; i++){
+        if(array[i] > arrayMaximo){
+            arrayMaximo = array[i];
+        }
+    }return arrayMaximo;
+}
+
 const cantidadDePersonas = Number(prompt('Cuantas personas son en tu grupo familiar?'));
 const nodoPagina = document.querySelector('body');
 
@@ -21,4 +51,35 @@ for(let i=1; i<=cantidadDePersonas; i++){
     nodoPagina.appendChild(crearLabel().appendChild(nuevoTexto));
     nodoPagina.appendChild(crearInput());
     nodoPagina.appendChild(crearBr());
+}
+
+
+document.querySelector('button').onclick = function(){
+    const nodeList = document.querySelectorAll('input');
+    const array = [];
+    for(let i=0; i<nodeList.length; i++){
+        if(Number(nodeList[i].value) === 0){
+            continue;
+        }
+        array.push(Number(nodeList[i].value));
+    }
+    const nodoPagina = document.querySelector('body');
+    const nuevoParrafo = document.createElement('p');
+    const nuevoEm = document.createElement('em');
+    const texto1 = document.createTextNode(`El promedio es ${calcularPromedioArray(array)}`);
+    const texto2 = document.createTextNode(`La edad minima es ${obtenerMinimo(array)}`);
+    const texto3 = document.createTextNode(`La edad maxima es ${obtenerMaximo(array)}`);
+    nuevoEm.appendChild(texto1);
+    nuevoEm.appendChild(crearBr());
+    nuevoEm.appendChild(texto2);
+    nuevoEm.appendChild(crearBr());
+    nuevoEm.appendChild(texto3);
+    nuevoEm.appendChild(crearBr());
+    nuevoParrafo.appendChild(nuevoEm);
+    nodoPagina.appendChild(nuevoParrafo);
+    return false;
+}
+
+document.querySelector('#reset').onclick = function(){
+    window.location.reload();
 }
